86234ac9b30222eb2a1b133ddd994094543b04a6
Deepak Ramalingam <rdeepak2002@gmail.com>
2024-04-21 19:34:02 -0700

    Use different URL for production

--- src/api/device.js
+++ src/api/device.js
@@ -1,6 +1,7 @@
 const API_HOST = import.meta.env.MODE === 'production' ? "https://device-status-server-b73fe300040d.herokuapp.com" : "http://localhost:8081";
-console.debug("Meta ENV MODE", import.meta.env.MODE);
-console.debug("Using API_HOST", API_HOST);
+console.log("Meta ENV MODE", import.meta.env.MODE);
+console.log("Using API_HOST", API_HOST);
+
 /**
  * Get all devices
  * @returns {Promise<Response>}
